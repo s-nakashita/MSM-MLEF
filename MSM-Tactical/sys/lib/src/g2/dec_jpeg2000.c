785b354b1f726e57c8a0f9a14f73278eca965b09
nakashita <>
2023-02-09 04:36:20 +0000

    environmental settings & intermediate files removed
    FossilOrigin-Name: f817e33ac06fb1cf23f12b37b68d2070af7a1168e96818d2d780565e5c211da9

--- MSM-Tactical/sys/lib/src/g2/dec_jpeg2000.c
+++ MSM-Tactical/sys/lib/src/g2/dec_jpeg2000.c
@@ -84,9 +84,9 @@
 //   
 //     Decode JPEG200 codestream into jas_image_t structure.
 //       
-    image=jpc_decode(jpcstream,opts);
+    image=jas_image_decode(jpcstream,jas_image_strtofmt("jpc"),opts);
     if ( image == 0 ) {
-       printf(" jpc_decode return = %d \n",ier);
+       printf(" jas_image_decode return = %d \n",ier);
        return -3;
     }
     
